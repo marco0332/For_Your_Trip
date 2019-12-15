8e73edd584c446c7fb8f3f4b7d35b3b93edc1e0e
marco0332 <marco2520332@gmail.com>
2019-12-15 18:49:50 +0900

    [ADD] Init Project Setting

--- backend/src/main/java/com/forb/backend/BackendApplication.java
+++ backend/src/main/java/com/forb/backend/BackendApplication.java
@@ -2,8 +2,11 @@ package com.forb.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class BackendApplication {
 
 	public static void main(String[] args) {
