313169ade2eda3cbed871976efd12c37ad105755
sujinhope <hope772948@gmail.com>
2019-12-16 09:55:28 +0900

    [add] Vue

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/view/Home'
+import Content from '@/view/Content'
+import Roadmap from '@/view/Roadmap'
 
 Vue.use(Router)
 
@@ -9,8 +11,18 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/content',
+      name: 'Content',
+      component: Content
+    },
+    {
+      path: '/roadmap',
+      name: 'Roadmap',
+      component: Roadmap
     }
   ]
 })
---
+++ frontend/src/view/Roadmap.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+      <div class='roadmap'>
+        <h1>roadmap</h1>
+        <div class='roadmap_contents'>
+          This is RealRoadmap
+        </div>
+      </div>
+      <div class='curriculum'>
+        <h1>curriculum</h1>
+        <div class='list'>
+          <h5>Frontend</h5>
+          <ul>
+            <li>HTML5</li>
+            <li>CSS3</li>
+            <li>Javascript</li>
+          </ul>
+          <h5>Backend</h5>
+          <ul>
+            <li>MySQL</li>
+          </ul>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .roadmap {
+        width: 100%;
+        height: 800px;
+        margin-top: 20px;
+        margin-bottom: 20px;
+        padding-top: 20px;
+        padding-bottom: 20px;
+        background-color:#555555
+    }
+    .curriculum {
+        width: 100%;
+        height: 500px;
+        margin: 20px 0 20px 0;
+        padding: 20px;
+        background-color: cornflowerblue;
+    }
+    .list {
+        text-align:start
+    }
+    .roadmap_contents {
+      margin:auto 20px;
+      background-color:azure;
+      height:90%;
+    }
+
+</style>
