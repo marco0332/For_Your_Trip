aa0f97547d2ce2beee944ea0dffa67125a8507db
marco0332 <marco2520332@gmail.com>
2019-12-17 22:11:54 +0900

    [ADD] Add sub Nav in lectureList

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -1,15 +1,32 @@
 <template>
   <v-app id="nav">
     <v-toolbar dark
+               height="64px"
                absolute
                scroll-off-screen>
       <v-toolbar-items>
-        <v-btn flat>Home</v-btn>
-        <v-btn flat>Git</v-btn>
-        <v-btn flat>강의 목록</v-btn>
-        <v-btn flat>정보통</v-btn>
-        <v-btn flat>질문게시판</v-btn>
-        <v-btn flat>로그인</v-btn>
+        <v-btn id="toHome" flat>Home</v-btn>
+        <v-btn id="toGit" flat>Git</v-btn>
+        <v-menu open-on-hover
+                dark
+                origin="top left"
+                transition="slide-y-transition">
+          <template v-slot:activator="{ on }">
+            <v-btn id="toLectureList" flat v-on="on">
+              Class & Roadmap
+            </v-btn>
+          </template>
+          <div id="lectureList">
+            <p>testsetst</p>
+            <p>testsetst</p>
+            <p>testsetst</p>
+            <p>testsetst</p>
+            <p>testsetst</p>
+          </div>
+        </v-menu>
+        <v-btn id="toInfoTong" flat>정보통</v-btn>
+        <v-btn id="toBoard" flat>질문게시판</v-btn>
+        <v-btn id="toLogin" flat>로그인</v-btn>
       </v-toolbar-items>
     </v-toolbar>
   </v-app>
@@ -22,8 +39,20 @@ export default {
 </script>
 
 <style scoped>
-#nav2 {
-    position: fixed;
-    height: 64px;
+#nav {
+  height: 64px;
+}
+
+div.v-menu__content {
+  background-color: #212121;
+  z-index: 0!important;
+}
+
+#lectureList {
+  position: relative;
+  margin-top: 64px;
+  margin-bottom: 0;
+  background-color:#212121;
+  color: white;
 }
 </style>
