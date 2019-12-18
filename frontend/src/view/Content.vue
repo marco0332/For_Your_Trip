19b61cf7b5bc149eed08a4198f29860bf315f448
sujinhope <hope772948@gmail.com>
2019-12-18 09:29:12 +0900

    [add] Layout

--- frontend/src/view/Content.vue
+++ frontend/src/view/Content.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        
-
+    <div class='box'>
+        <div class='classNavigation'>
+            <b>classNavigation</b>
+        </div>
+        <div class='contents'>
+            <b>contents</b>
+        </div>
     </div>
 </template>
 
@@ -11,6 +15,37 @@ export default {
 }
 </script>
 
-<style>
+<style scopped>
+
+    .box {
+        border: 1px solid darkcyan;
+        margin: 0 auto;
+        margin-top: 50px;
+        margin-bottom: 50px;
+        position: relative;
+        /* float: left; */
+        /* display: flex; */
+        width: 95%;
+        min-width: 1400px;
+    }
+
+    .classNavigation {
+        border: 1px solid black;
+        position: fixed;
+        display: table-cell;
+        margin: 30px;
+        width: 300px;
+        height: 500px;
+    }
+    .contents {
+        border: 1px solid burlywood;
+        position: absolute;
+        display: table-cell;
+        margin-left: 400px;
+        margin-top: 30px;
+        width: 70%;
+        min-width: 800px; 
+        height: 1000px;
+    }
 
 </style>
