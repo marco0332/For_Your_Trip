8fe599c44a12a8a90558307d2c72a2f4446c5f13
marco0332 <marco2520332@gmail.com>
2019-12-30 17:26:54 +0900

    [MERGE] and [ADD]

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <v-flex v-if="!bAuthentication">
+            <!-- Google Login -->
+            <v-btn round color="#df4a31"
+                    dark
+                    @click="loginWithGoogle"
+                    style="width:100%;">
+                <v-icon size="25" class="mr-2">fa-google</v-icon>Google 로그인
+            </v-btn>
+        </v-flex>
+        <v-flex v-else>
+            <v-icon size="100">accessbility_new</v-icon>
+            <v-card-text>Welcome!</v-card-text>
+        </v-flex>
+    </div>
+</template>
+
+<script>
+import FirebaseService from "@/plugins/FirebaseService"
+import { mapState, mapMutations, mapActions } from "vuex"
+
+export default {
+  name: 'loginPage',
+  computed: {
+    ...mapState(["bAuthentication"])
+  },
+  methods: {
+      ...mapActions(["loginWithOauth"]),
+      ...mapMutations(["fillUser"]),
+      loginWithGoogle() {
+        this.loginWithOauth("google")
+        this.$router.push({ name: "Home" })
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/plugins/FirebaseService.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app"
+import "firebase/firestore"
+import "firebase/auth"
+import storage from "firebase/storage"
+import store from "./store"
+
+const USER = "user"
+
+const config = {
+
+}
+firebase.initializeApp(config)
